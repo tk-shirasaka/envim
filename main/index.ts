da6b1ec5d2a3b92c60deec4d2635f39045b9a21f
shirasaka <shirasaka@n-create.co.jp>
2020-01-14 12:24:33 +0900

    Remote not need function

--- main/index.ts
+++ main/index.ts
@@ -1,7 +1,5 @@
 import { Browser } from "./browser";
-import { Setting } from "./setting";
 import { Envim } from "./envim";
 
 new Browser;
-new Setting;
 new Envim;
--- renderer/components/setting.tsx
+++ renderer/components/setting.tsx
@@ -1,5 +1,5 @@
 import React, { FormEvent, ChangeEvent } from "react";
-import { ipcRenderer, IpcRendererEvent } from "electron";
+import { ipcRenderer } from "electron";
 
 import { Localstorage } from "../utils/localstorage";
 import { icons } from "../utils/icons";
@@ -48,19 +48,11 @@ const styles = {
 };
 
 export class SettingComponent extends React.Component<Props, States> {
-  private timeout: number = 0;
-  private prevCommand: string = "";
   private ls: Localstorage<States> = new Localstorage<States>("setting", { type: "command", command: "", address: "", commandList: [] });
 
   constructor(props: Props) {
     super(props);
     this.state = this.ls.get();
-
-    ipcRenderer.on("setting:command-list", this.onCommandList.bind(this));
-  }
-
-  componentWillUnmount() {
-    ipcRenderer.removeAllListeners("setting:command-list");
   }
 
   private onToggle(e: ChangeEvent) {
@@ -70,27 +62,12 @@ export class SettingComponent extends React.Component<Props, States> {
 
   private onChangeCommand(e: ChangeEvent) {
     const command = (e.target as HTMLInputElement).value;
-
-    if (this.state.type === "command") {
-      const timeout = +setTimeout(() => {
-        if (command === this.prevCommand) return;
-        if (!(command && timeout === this.timeout)) return;
-        this.timeout = 0;
-        this.prevCommand = command;
-        ipcRenderer.send("setting:command", command);
-      }, 500);
-      this.timeout = timeout;
-    }
     this.setState({ command });
   }
 
   private onChangePort(e: ChangeEvent) {
     const address = (e.target as HTMLInputElement).value;
     this.setState({ address });
-  }
-
-  private onCommandList(_: IpcRendererEvent, list: string[]) {
-    this.setState({commandList: list});
   }
 
   private onSubmit(e: FormEvent) {
@@ -109,12 +86,9 @@ export class SettingComponent extends React.Component<Props, States> {
           <label style={styles.label}><input style={styles.radio} type="radio" value="address" checked={this.state.type === "address"} onChange={this.onToggle.bind(this)} />Port</label>
         </div>
         {this.state.type === "command"
-          ?  <label style={styles.label}>Enter neovim command<input style={styles.text} value={this.state.command} list="command-list" onChange={this.onChangeCommand.bind(this)} autoFocus={true} /></label>
+          ?  <label style={styles.label}>Enter neovim command<input style={styles.text} value={this.state.command} onChange={this.onChangeCommand.bind(this)} autoFocus={true} /></label>
           :  <label style={styles.label}>Enter neovim address<input style={styles.text} value={this.state.address} onChange={this.onChangePort.bind(this)} autoFocus={true} /></label>
         }
-        <datalist id="command-list">
-          {this.state.commandList.map(command => <option key={`list_${command}`} value={command} />)}
-        </datalist>
         <div>
           {icons.map(icon => <i key={icon.name} style={{color: icon.color, ...styles.i}}>{icon.font}</i>)}
         </div>
