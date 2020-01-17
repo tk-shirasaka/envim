9dd56df6533daffed0cffcc2767520d025632934
shirasaka <shirasaka@n-create.co.jp>
2020-01-17 18:26:07 +0900

    Improve highlight

--- renderer/utils/context2d.ts
+++ renderer/utils/context2d.ts
@@ -1,15 +1,4 @@
-interface Highlight {
-  foreground: number;
-  background: number;
-  special: number;
-  reverse: boolean;
-  italic: boolean;
-  bold: boolean;
-  strikethrough: boolean;
-  underline:boolean;
-  undercurl:boolean;
-  blend: number;
-}
+import { Highlight } from "./highlight";
 
 export class Context2D {
   private highlights: { [k: number]: Highlight } = {};
@@ -20,32 +9,19 @@ export class Context2D {
     private font: { size: number; width: number; height: number; },
   ) { }
 
-  private intToColor(color: number) {
-    return `#${("000000" + color.toString(16)).slice(-6)}`;
-  }
-
   private style(hl: number, type: "foreground" | "background") {
-    const foreground = this.highlights[hl].foreground || this.highlights[0].foreground;
-    const background = this.highlights[hl].background || this.highlights[0].background;
-    const reverse = this.highlights[hl].reverse;
-
-    switch (type) {
-      case "foreground": return (this.ctx.fillStyle = this.intToColor(reverse ? background : foreground));
-      case "background": return (this.ctx.fillStyle = this.intToColor(reverse ? foreground : background));
-    }
+    this.ctx.fillStyle = this.highlights[hl][type] || this.highlights[0][type];
   }
 
   private fontStyle(hl: number) {
     this.ctx.textBaseline = "top";
-    this.ctx.font = `${this.font.size}px Ricty Diminished, Nerd Font`;
-    this.highlights[hl].bold && (this.ctx.font = `${this.font.size}px Ricty Diminished Bold, Nerd Font Bold`);
-    this.highlights[hl].italic && (this.ctx.font = `${this.font.size}px Ricty Diminished Oblique`);
+    this.ctx.font = `${this.font.size}px ${this.highlights[hl].font}`;
   }
 
   private underline(x: number, y: number, col: number, hl: number) {
-    if (this.highlights[hl].underline || this.highlights[hl].undercurl) {
+    if (this.highlights[hl].decoration) {
       this.ctx.save();
-      this.ctx.strokeStyle = this.intToColor(this.highlights[hl].special || this.highlights[0].special);
+      this.ctx.strokeStyle = this.highlights[hl].special || this.highlights[0].special;
       this.ctx.beginPath();
       this.ctx.moveTo(x, y + this.font.height - 1);
       this.ctx.lineTo(x + col * this.font.width, y + this.font.height - 1);
@@ -65,8 +41,8 @@ export class Context2D {
     this.font = font;
   }
 
-  setHighlight(id: number, hl: Highlight) {
-    this.highlights[id] = hl;
+  setHighlight(id: number, hl: object) {
+    this.highlights[id] = new Highlight(hl);
   }
 
   setCursor(x: number, y: number, hl: number) {
---
+++ renderer/utils/highlight.ts
@@ -0,0 +1,58 @@
+export class Highlight {
+  public foreground: string = "";
+  public background: string = "";
+  public special: string = "";
+  private type: "normal" | "bold" | "italic";
+  private decorate: "none" | "strikethrough" | "underline" | "undercurl";
+
+  public blend: number;
+
+  constructor(highlight :{
+    foreground?: number,
+    background?: number,
+    special?: number,
+    reverse?: boolean,
+    italic?: boolean,
+    bold?: boolean,
+    strikethrough?: boolean,
+    underline?: boolean,
+    undercurl?: boolean,
+    blend?: number,
+  }) {
+    if (highlight.reverse) {
+      highlight.foreground && (this.background = this.intToColor(highlight.foreground));
+      highlight.background && (this.foreground = this.intToColor(highlight.background));
+    } else {
+      highlight.foreground && (this.foreground = this.intToColor(highlight.foreground));
+      highlight.background && (this.background = this.intToColor(highlight.background));
+    }
+    highlight.special && (this.special = this.intToColor(highlight.special));
+
+    this.type = "normal";
+    if (highlight.bold) this.type = "bold";
+    if (highlight.italic) this.type = "italic";
+
+    this.decorate = "none";
+    if (highlight.strikethrough) this.decorate = "strikethrough";
+    if (highlight.underline) this.decorate = "underline";
+    if (highlight.undercurl) this.decorate = "undercurl";
+
+    this.blend = highlight.blend || 0;
+  }
+
+  private intToColor(color: number) {
+    return `#${("000000" + color.toString(16)).slice(-6)}`;
+  }
+
+  get font() {
+    return {
+      normal: "Ricty Diminished, Nerd Font",
+      bold: "Ricty Diminished Bold, Nerd Font Bold",
+      italic: "Ricty Diminished Oblique",
+    }[this.type];
+  }
+
+  get decoration() {
+    return ["underline", "undercurl"].indexOf(this.decorate) >= 0;
+  }
+}
