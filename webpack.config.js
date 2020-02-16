f83ee241abdeb396c9ef3d68d1ed537b827d91a9
Shirasaka <tk.shirasaka@gmail.com>
2020-02-17 00:24:53 +0900

    Change styling language css to scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 var main = {
@@ -32,26 +33,39 @@ var main = {
 var renderer = {
   mode: 'development',
   target: 'electron-renderer',
-  entry: path.join(__dirname, 'renderer', "components", 'index'),
+  entry: path.join(__dirname, 'renderer', 'components', 'index'),
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'renderer')
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
+    extensions: ['.js', '.jsx', '.ts', '.tsx', 'scss']
   },
   module: {
-    rules: [{
-      test: /\.(tsx|ts)$/,
-      use: [
-        'ts-loader'
-      ],
-      include: [
-        path.resolve(__dirname, 'renderer'),
-        path.resolve(__dirname, 'node_modules'),
-      ],
-    }]
+    rules: [
+      {
+        test: /\.(tsx|ts)$/,
+        use: [
+          'ts-loader'
+        ],
+        include: [
+          path.resolve(__dirname, 'renderer'),
+          path.resolve(__dirname, 'node_modules'),
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { url: false } },
+          'sass-loader',
+        ],
+      },
+    ]
   },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: '../styles.css' }),
+  ],
 };
 
 module.exports = [
