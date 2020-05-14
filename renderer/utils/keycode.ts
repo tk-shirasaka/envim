89041d276c2a714633a18ed6a7f21f61ce8d82f1
Shirasaka <tk.shirasaka@gmail.com>
2020-05-15 00:05:30 +0900

    Improve keycodes

--- renderer/utils/keycode.ts
+++ renderer/utils/keycode.ts
@@ -22,9 +22,27 @@ export const keycode = (e: KeyboardEvent) => {
     case "PageDown": return "<PageDown>";
     case "Help": return "<Help>";
     case "CapsLock": return "";
+    case "Alphanumeric": return "";
+    case "NonConvert": return "";
+    case "Convert": return "";
+    case "HiraganaKatakana": return "";
+    case "Clear": return "";
+    case "NumLock": return "";
     case "Control": return "";
     case "Shift": return "";
     case "Alt": return "";
+    case "F1": return "<F1>";
+    case "F2": return "<F2>";
+    case "F3": return "<F3>";
+    case "F4": return "<F4>";
+    case "F5": return "<F5>";
+    case "F6": return "<F6>";
+    case "F7": return "<F7>";
+    case "F8": return "<F8>";
+    case "F9": return "<F9>";
+    case "F10": return "<F10>";
+    case "F11": return "<F11>";
+    case "F12": return "<F12>";
   }
 
   if (keyCode === 229) return "";
