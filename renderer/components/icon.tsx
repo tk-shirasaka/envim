88be0b0be85817d381e8814742bac4360163a594
shirasaka <shirasaka@n-create.co.jp>
2020-06-19 18:15:38 +0900

    Keep right rate for text and icon

--- renderer/components/icon.tsx
+++ renderer/components/icon.tsx
@@ -17,7 +17,7 @@ export class IconComponent extends React.Component<Props, States> {
   render() {
     const { size } = Setting.font;
     const className = `color-${this.props.color} ${this.props.active ? "active" : this.props.onClick ? "clickable" : ""}`;
-    const style = {fontSize: size + 8, lineHeight: `${size + 1}px`, ...this.props.style};
+    const style = {fontSize: size / 2 * 3, lineHeight: `${size + 1}px`, ...this.props.style};
 
     return (
       <i {...{ className, style }} onClick={this.props?.onClick}>
