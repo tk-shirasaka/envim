2961ed7e3bb11ce4346edfcd0416dffa6be7dd0b
Shirasaka <tk.shirasaka@gmail.com>
2021-09-09 00:20:52 +0900

    Fixed color difference detection

--- main/envim/highlight.ts
+++ main/envim/highlight.ts
@@ -9,12 +9,12 @@ export class Highlights {
     if (hl.foreground) highlight.fg = hl.foreground;
     if (hl.background) highlight.bg = hl.background;
     if (hl.special) highlight.sp = hl.special;
-    if (hl.bold) highlight.fg = 0x2000000 | highlight.fg;
-    if (hl.italic) highlight.fg = 0x4000000 | highlight.fg;
-    if (ui) highlight.bg = 0x2000000 | highlight.bg;
-    if (hl.strikethrough) highlight.sp = 0x2000000 | highlight.sp;
-    if (hl.underline) highlight.sp = 0x4000000 | highlight.sp;
-    if (hl.undercurl) highlight.sp = 0x8000000 | highlight.sp;
+    if (hl.bold) highlight.fg = 0x04000000 | highlight.fg;
+    if (hl.italic) highlight.fg = 0x08000000 | highlight.fg;
+    if (ui) highlight.bg = 0x04000000 | highlight.bg;
+    if (hl.strikethrough) highlight.sp = 0x04000000 | highlight.sp;
+    if (hl.underline) highlight.sp = 0x08000000 | highlight.sp;
+    if (hl.undercurl) highlight.sp = 0x10000000 | highlight.sp;
 
     [ highlight.fg, highlight.bg ] = hl.reverse ? [ highlight.bg, highlight.fg ] : [ highlight.fg, highlight.bg ];
 
@@ -22,6 +22,6 @@ export class Highlights {
   }
 
   static get(id: string) {
-    return Highlights.hls[id] || { fg: 0x1000000, bg: 0x1000000, sp: 0x1000000 };
+    return Highlights.hls[id] || { fg: 0x01000000, bg: 0x02000000, sp: 0x03000000 };
   }
 }
