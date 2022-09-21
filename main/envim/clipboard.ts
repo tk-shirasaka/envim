1c477168d5afad61f37ffb4c3db28718946825c7
shirasaka <tk.shirasaka@gmail>
2022-09-22 02:59:08 +0900

    Open link to browser

--- main/envim/clipboard.ts
+++ main/envim/clipboard.ts
@@ -1,11 +1,13 @@
-import { clipboard } from "electron";
+import { dialog, clipboard, BrowserWindow } from "electron";
 import { Response } from "neovim/lib/host";
 
+import { Browser } from "../browser";
 import { Emit } from "../emit";
 
 export class Clipboard {
   private static lines: string[];
   private static type: "v" | "V" | "b";
+  private static win?: BrowserWindow;
 
   static setup(channel: number) {
     Emit.share("envim:command", [
@@ -29,6 +31,10 @@ export class Clipboard {
     Clipboard.lines = lines;
     Clipboard.type = type
     clipboard.writeText(lines.join("\n"));
+
+    if (lines[0].search(/^https?:\/\/\w+/) === 0 && Browser.win) {
+      Clipboard.openUrl(lines[0]);
+    }
   }
 
   static paste(res: Response) {
@@ -38,6 +44,20 @@ export class Clipboard {
       res.send([Clipboard.lines, Clipboard.type]);
     } else {
       res.send([lines, lines.length > 1 ? "V" : "v"]);
+    }
+  }
+
+  private static openUrl(url: string) {
+    const options = { message: "Open a url?", buttons: ["Yes", "no"], defaultId: 0 };
+    if (dialog.showMessageBoxSync(options) === 0) {
+      if (!Clipboard.win) {
+        Clipboard.win = new BrowserWindow({ parent: Browser.win, show: false });
+        Clipboard.win.on("ready-to-show", () => Clipboard.win?.show());
+        Clipboard.win.on("close", () => delete(Clipboard.win));
+        Clipboard.win.webContents.session.clearStorageData();
+      }
+
+      Clipboard.win.loadURL(url);
     }
   }
 }
