e1da9a60de2c187501a365a71b4e5a4c491dd4f9
shirasaka <tk.shirasaka@gmail>
2023-06-03 00:50:31 +0900

    Fix script error

--- lua/clipboard.lua
+++ lua/clipboard.lua
@@ -1,9 +1,9 @@
 local function copy(lines, regtype)
-  envim(0, { "envim_clipboard", lines, regtype })
+  return envim(0, { "envim_clipboard", lines, regtype })
 end
 
 local function paste()
-  envim(1, { "envim_clipboard" })
+  return envim(1, { "envim_clipboard" })
 end
 
 vim.g.loaded_clipboard_provider = nil
